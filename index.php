009240f63054c92338866a9f5ee9d513f7612b79
giuseppe <placidagiuseppe@gmail.com>
2021-04-30 17:00:34 +0200

    ...

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+  $array = [
+    'prima' => [
+        [
+            'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+            'paragraph_1' => "La recente <span class='select'> decisione della Corte di giustizia dell'Unione europea </span> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+            'paragraph_2' => "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
+
+            'paragraph_4' => "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
+            // 'paragraph_5' => "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione. "
+        ],
+  ];
+
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="./assets/master.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
+  </head>
+  <body>
+    <header>
+      <nav>
+        <div class="nav-bar-1">
+          <div class="left">
+            <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
+            <p>Privacy e termini</p>
+          </div>
+          <div class="right">
+            <i class="fas fa-expand-arrows-alt"></i>
+            <div class="circle">
+              <p>G</p>
+            </div>
+          </div>
+        </div>
+        <div class="nav-bar-2">
+          <ul>
+
+            <li><a href="#">Introduzione</a></li>
+            <li><a href="#">Norme sulla privacy</a></li>
+            <li><a href="#">Termini di servizio</a></li>
+            <li><a href="#">Tecnologie</a></li>
+            <li><a class="select" href="#">Domande frequenti</a></li>
+          </ul>
+        </div>
+
+      </nav>
+    </header>
+
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+
+    <footer></footer>
+  </body>
+</html>
